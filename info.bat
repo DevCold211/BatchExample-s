a91dee3b9f433b12c57fde24326ceecba38a083e
DevCold211 <38896652+DevCold211@users.noreply.github.com>
2018-06-22 16:40:43 -0400

    Fixed Menu Changed, Add New BATCH with it

---
+++ info.bat
@@ -0,0 +1,11 @@
+cls
+@echo off
+title Info
+color 0d
+
+echo () = Command, {} = Put There
+echo YT: Wolfy Playz
+echo Website: https://devcold211.github.io/wolfplayz/
+echo Description Of This: Example of simple batch compents and can fix exe problems like make a batch file and start make it start google up you (start {url})
+pause >nul
+call "Menu.bat"
