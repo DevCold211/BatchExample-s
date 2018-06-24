5e3154c45013ec0ad238c412c0faf48c63e455fa
DevCold211 <38896652+DevCold211@users.noreply.github.com>
2018-06-24 14:36:10 -0400

    having function problems

---
+++ src/Settings.bat
@@ -0,0 +1,19 @@
+cls
+@echo off
+title Settings
+color 3
+
+echo Working ON MORE
+echo 1 ) Help
+
+set /p num=Number: 
+
+if %num% == 1 (
+goto help
+)
+
+:help
+echo Survie the night by check the camraes and hideing under your desk but not all the time 
+echo will it work sometimes it might not work and the robots try to well stuff you in a freddy fazbear
+echo suits it takes 3 min in real life for 1 night at freddy there is a 80% chance you can win
+echo and a 20% chance you wont!
